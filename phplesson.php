3e8ae228c10e70e426684cbadd4a59e1143643cf
blu-men <cjbb5eyv@icloud.com>
2021-07-24 01:45:11 +0900

    %d_is_position_of_params

--- phplesson.php
+++ phplesson.php
@@ -1,16 +1,10 @@
 <?php
 
-  $hunberger = 4.95;
-  $chocoMilkShake = 1.95;
-  $cola = 0.85;
-
-  $sumEat = 2 * $hunberger + $chocoMilkShake + $cola;
-
-  $tax = $sumEat * 0.075;
-  $chipDinner = $sumEat * 0.16;
-
-  $sumAll = $sumEat + $tax + $chipDinner;
-
-  print $sumAll;
+  $f = -50;
+  while ($f <= 50) {
+    $c = ($f - 32) * (5/9);
+    printf( "%d degrees F = %d degrees C\n" ,$f, $c);
+    $f += 5;
+  }
 
 ?>
